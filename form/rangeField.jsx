7b079fad9957249738a43b37325b00150babfc44
k5man <alexandrov.dmitri@gmail.com>
2018-04-12 14:15:46 +0300

    Forms | Range Input

--- form/rangeField.jsx
+++ form/rangeField.jsx
@@ -4,26 +4,14 @@ import BaseInput from './baseInput';
 export default class IncrimentField extends BaseInput {
 			  
 	render() {
-		function updateTextInput(val) {
-		          document.getElementById('textInput').value='1'; 
-		        }
-		if (!this.props.id) {
-			this.props.id = (Date.now() + Math.random()).toString(25).replace('.', '');
-		}
-		console.log('PROPS: ',this.props);
 		return (
 			<p class="range-field">
-				<input type="range" id='rangeInput' min="0" max="100" />
+				<input 
+					type="range"
+					{...this.getCustomProps()}
+				/>
 			</p>
 		);
 	}
 
 }
-
-// <p className={`checkbox-field ${this.props.className}`} >
-// 	<input
-// 		type="checkbox"
-// 		{...this.getCustomProps()}
-// 	/>
-// 	{this.props.label ? <label htmlFor={this.props.id}>{this.props.label}</label> : null}
-// </p>
